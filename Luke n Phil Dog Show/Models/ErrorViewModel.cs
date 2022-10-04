fb2d3e18bd5d18e394a7c9e9c9b865a21df37d60
Lucas Seeley <69803592+LucasSeeley@users.noreply.github.com>
2022-10-03 21:36:27 -0400

    Add project files.

---
+++ Luke n Phil Dog Show/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Luke_n_Phil_Dog_Show.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
