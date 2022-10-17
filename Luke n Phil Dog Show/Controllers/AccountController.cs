18c0af58584999a60095f3a816f8e0cf4b3e9005
Lucas Seeley <69803592+LucasSeeley@users.noreply.github.com>
2022-10-17 17:34:16 -0400

    register user

---
+++ Luke n Phil Dog Show/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using Luke_n_Phil_Dog_Show.Models;
+using Luke_n_Phil_Dog_Show.View_Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Luke_n_Phil_Dog_Show.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Register(string? returnUrl = null)
+        {
+            RegisterViewModel registerViewModel = new RegisterViewModel();
+            registerViewModel.ReturnUrl = returnUrl;
+            return View(registerViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel, string? returnUrl = null)
+        {
+            registerViewModel.ReturnUrl = returnUrl;
+            returnUrl = returnUrl ?? Url.Content("~/");
+            if (ModelState.IsValid)
+            {
+                var user = new AppUser { Email = registerViewModel.Email, UserName = registerViewModel.UserName };
+                var result = await _userManager.CreateAsync(user, registerViewModel.Password);
+                if (result.Succeeded)
+                { 
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return LocalRedirect(returnUrl);
+                }
+                ModelState.AddModelError("Password", "User could not be created. Password not unique enough");
+            }
+            return View(registerViewModel);
+        }
+    }
+}
